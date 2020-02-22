380d88f779a02b2ef400689d23110d9ebef333f9
Nick Gorajski <nick_gorajski@bcbsil.com>
2020-02-21 20:34:16 -0600

    Load schema and data into H2

---
+++ database-demo/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO person
+    (ID, NAME, LOCATION, BIRTH_DATE)
+VALUES(10001,'Ranga','Hyderabad',sysdate());
+INSERT INTO person
+    (ID, NAME, LOCATION, BIRTH_DATE)
+VALUES(10002,'James','New York',sysdate());
+INSERT INTO person
+    (ID, NAME, LOCATION, BIRTH_DATE)
+VALUES(10003,'Pieter','Amsterdam',sysdate());
